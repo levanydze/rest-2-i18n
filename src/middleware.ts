5c3c4ada78f0221867a968be6dc66bb7dfb4fcfa
levanydze <66974840+levanydze@users.noreply.github.com>
2024-09-27 15:12:45 +0200

    1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,13 @@
 import createMiddleware from "next-intl/middleware";
+import { supportedLocales, defaultLocale } from "@/Manager/navigation";
 
 export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ["en", "de", "fr", "it"],
-
-  // Used when no locale matches
-  defaultLocale: "en",
+  locales: supportedLocales,
+  defaultLocale: defaultLocale,
 });
 
 export const config = {
-  // Match only internationalized pathnames
   matcher: ["/", "/(de|en|fr|it)/:path*"],
 };
+
+//must fix
