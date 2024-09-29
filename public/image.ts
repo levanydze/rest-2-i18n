e589a52b0ed29329fa38d245bd0dc0b46a7854d0
levanydze <66974840+levanydze@users.noreply.github.com>
2024-09-29 19:38:05 +0200

    1

--- public/image.ts
+++ public/image.ts
@@ -1,28 +1,28 @@
 //
 //
 // hero images
-import heroMain from "@/public/images/homePage/hero.jpg";
-import heroMenu from "@/public/images/homePage/ComponentTwo.jpg";
-import heroStory from "@/public/images/homePage/ComponentSix.jpg";
-import heroReservation from "@/public/images/homePage/ComponentThree.jpg";
+import heroMain from "@/public/images/heros/heroHome.jpg";
+import heroMenu from "@/public/images/heros/heroMenu.jpg";
+import heroStory from "@/public/images/heros/heroStory.jpg";
+import heroReservation from "@/public/images/heros/heroReservation.jpg";
 
 //
 //
 //homepage Images
-import croisant from "@/public/images/homePage/ComponentTwo.jpg";
-import pizza from "@/public/images/homePage/ComponentThree.jpg";
-import pizza2 from "@/public/images/homePage/ComponentSix.jpg";
+import croisant from "@/public/images/heros/heroHome.jpg";
+import pizza from "@/public/images/ComponentThree.jpg";
+import pizza2 from "@/public/images/ComponentSix.jpg";
 
-import instagram1 from "@/public/images/homePage/hero.jpg";
-import instagram2 from "@/public/images/homePage/ComponentTwo.jpg";
-import instagram3 from "@/public/images/homePage/ComponentSix.jpg";
-import instagram4 from "@/public/images/homePage/ComponentThree.jpg";
+import instagram1 from "@/public/images/heros/heroHome.jpg";
+import instagram2 from "@/public/images/heros/heroMenu.jpg";
+import instagram3 from "@/public/images/heros/heroReservation.jpg";
+import instagram4 from "@/public/images/heros/heroStory.jpg";
 
 //
 //
 //images story page
-import chefImage from "@/public/images/storyPage/sushiChef.png";
-import chroisant2 from "@/public/images/homePage/ComponentTwo.jpg";
+import chefImage from "@/public/images/sushiChef.png";
+import chroisant2 from "@/public/images/heros/heroReservation.jpg";
 import logo from "@/public/assets/svg/Puki..svg";
 import logoDark from "@/public/assets/svg/Puki-dark.svg";
 
