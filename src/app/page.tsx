da053be865b4aae420a982916912c6c23e2bd7e8
levanydze <66974840+levanydze@users.noreply.github.com>
2024-09-30 12:19:30 +0200

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import { defaultLocale } from "@/Manager/navigation";
-import { useRouter } from "next/router";
-
 export default function IndexPage() {
-  const router = useRouter();
-
-  return (
-    <div
-      onClick={() => {
-        router.push("/", "/", { locale: defaultLocale });
-      }}
-    ></div>
-  );
+  return <div></div>;
 }
